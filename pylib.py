50c88cb0db0244b17beecbe3225bb6c525343ae3
nidashabbir4500-commit <nidashabbir4500@gmail.com>
2026-01-05 20:36:55 +0500

    Add files via upload

---
+++ pylib.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y)
+plt.xlabel("x-label")
+plt.ylabel("y-label")
+plt.title("Simple Line Plot")
+plt.show()

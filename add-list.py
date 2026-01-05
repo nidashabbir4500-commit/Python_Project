eaf49ce17beee773ee39a57c6e9a511b87fb9607
nidashabbir4500-commit <nidashabbir4500@gmail.com>
2026-01-05 20:50:57 +0500

    Add files via upload

---
+++ add-list.py
@@ -0,0 +1,14 @@
+#add list items
+#append()
+subjectlist=["ai","cn","ds"]
+subjectlist.append ("calculus")
+print (subjectlist)
+#insert()
+subjectlist=["ai","cn","ds"]
+subjectlist.insert (2,"calculus")
+print (subjectlist)
+#extend()
+subjectlist=["ai","cn","ds"]
+subjectmarks=[29,28,26]
+subjectlist.extend (subjectmarks)
+print(subjectlist)

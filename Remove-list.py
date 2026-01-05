c74e81a62c247bf163e9605b7423c15f7b4ca97d
nidashabbir4500-commit <nidashabbir4500@gmail.com>
2026-01-05 20:52:18 +0500

    Add files via upload

---
+++ Remove-list.py
@@ -0,0 +1,17 @@
+#remove list items
+#remove
+subjectlist=["ai","cn","ds"]
+subjectlist.remove ("cn")
+print (subjectlist)
+#pop
+subjectlist=["ai","cn","ds"]
+subjectlist.pop (2)
+print (subjectlist)
+#delete
+subjectlist=["ai","cn","ds"]
+del subjectlist [1]
+print (subjectlist)
+#remove
+subjectlist=["ai","cn","ds"]
+subjectlist.clear ()
+print (subjectlist)

1ed7e1ad4d543a9e417635b09216174cd6194688
nidashabbir4500-commit <nidashabbir4500@gmail.com>
2026-01-05 20:58:30 +0500

    Add files via upload

---
+++ Numpy_code.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+arr2=np.array([6,7,8,9,10])
+print(arr2)
+print("Addition of two array = ",arr+arr2)
+print("Multiply of two array = ",arr*arr2)

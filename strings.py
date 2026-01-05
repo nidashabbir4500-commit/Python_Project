a450e6ab67492797c7463dc9d628409c5c853285
nidashabbir4500-commit <nidashabbir4500@gmail.com>
2026-01-05 20:46:10 +0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+# check string and modify string method
+
+a = "my college"
+print(a.lower())
+print(a.upper())
+print(a.replace('m', 'z'))
+
+if "my" in a:
+    print("yes, 'my' in the string")
+elif "free" not in a:
+    print("no, 'free' not in the string")
